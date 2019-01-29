8af2c56347afcfa63e04da2f6aab0795191c59b8
William <lucyeni.we@gmail.com>
2019-01-29 15:50:42 +0100

    added timeout alert

--- countdown.js
+++ countdown.js
@@ -3,9 +3,16 @@ var number = prompt("Type in your countdown time (in seconds)" )
 
 const count = () =>{
 	
-	if(number > 0){number--;
+	if(number > 0){
+		number--;
+		console.log(number)}
 	
-	console.log(number)}
+	if (number === 0){
+		if(confirm(`Time's up!!!`)===true){
+			number=-1
+			}
+			//this ends the program
+		}
 	
 }
 	setInterval(count, 1000);
